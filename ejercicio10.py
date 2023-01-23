33281aa6c9bbd8fab05607d4e0265aa38c795444
guillermomasp <guillermomasp@gmail.com>
2023-01-23 09:05:43 +0100

    Potencia
    Le preguntamos al usuario

---
+++ ejercicio10.py
@@ -0,0 +1,5 @@
+#Le preguntamos al usuario que base y exponente quiere usar.
+print("¿Que numero quiere que sea su base?")
+base = int(input())
+print("¿Y su exponente?")
+exponente = int(input())
--- ejercicio11.py
+++ ejercicio11.py
@@ -6,3 +6,6 @@ def es_potencia_de_4(x):
         x = x / 4
     return x == 1
 
+#Le pedimos al usuario que nos mdiga su numero para hacer la comprobación
+print("¿Que número quiere comprobar?")
+x = int(input())
