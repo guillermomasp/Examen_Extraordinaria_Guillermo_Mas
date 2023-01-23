fe41359598d9f95e6fe2609ce4d10383bf06ee0c
guillermomasp <guillermomasp@gmail.com>
2023-01-23 10:22:25 +0100

    ej 1
    estoy probando a ver si saco algo a la desesperada :)

--- ejercicio1.py
+++ ejercicio1.py
@@ -13,3 +13,13 @@ class DotsAndBoxes(tk.Tk):
         self.create_grid()
         self.bind_events()
 
+    def create_grid(self):
+        for i in range(5):
+            self.canvas.create_line(50 * i, 0, 50 * i, 250)
+            self.canvas.create_line(0, 50 * i, 250, 50 * i)
+
+    def bind_events(self):
+        self.canvas.bind("<Button-1>", self.draw_line)
+
+
+
