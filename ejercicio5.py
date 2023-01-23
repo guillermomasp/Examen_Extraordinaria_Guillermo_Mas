b6d40c428fc459c36b1ee09d1e1bcf49000b0a9c
guillermomasp <guillermomasp@gmail.com>
2023-01-23 10:09:19 +0100

    ej 5
    creamos la segunda función para que haga el descifrado

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,9 +1,18 @@
 #Función para que haga el cifrado
 
-def cesar(chain,llave):
+def original(chain,key):
     chain = chain.lower()
-    key = llave.lower()
+    key = key.lower()
+    original = ""
+    for i in range(len(chain)):
+        original += chr((ord(chain[i]) + ord(key[i%len(key)]) - 2*ord('a'))%26 + ord('a'))
+    return original
+
+#Creamos la función del descifrado del Cesar
+def cesar(chain,key):
+    chain = chain.lower()
+    key = key.lower()
     cesar = ""
     for i in range(len(chain)):
-        cesar += chr((ord(chain[i]) + ord(llave[i%len(llave)]) - 2*ord('a'))%26 + ord('a'))
+        cesar += chr((ord(chain[i]) - ord(key[i%len(key)]) + 26)%26 + ord("a"))
     return cesar
