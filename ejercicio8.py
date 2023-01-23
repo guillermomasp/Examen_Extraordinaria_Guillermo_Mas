14b88e81d035222a6eaf7576f29528301562f52e
guillermomasp <guillermomasp@gmail.com>
2023-01-23 09:50:01 +0100

    ejercicio 8
    He terminado el ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -13,3 +13,7 @@ if _name_ == "_main_":
     lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     
     print(ordenar(lista))
+
+
+    #1 perdiz en un peral, 2 tortolas y , 3 gallinas francesas, 4 pájaros cantando, 5   anillos de oro, 6 gansos una puesta, 7 cisnes nadando, 8 críadas un ordeño,
+# 9 damas bailando , 10 señores de un salto, 11 gaiteros, 12 bateristas tocando el tambor
